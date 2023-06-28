3e1f7f9f11cdaeb7979406bbfd9d2c910a229101
stebf <stebf@mail.de>
2023-06-28 15:45:33 +0200

    [complements] Small improvements

--- complements/back_comp.py
+++ complements/back_comp.py
@@ -27,19 +27,27 @@ def ones_to_dec(raw_val: str) -> int:
         return bv
 
 
-def twos_to_dec(raw_val: str):
+def twos_to_dec(raw_val: str) -> int:
     """ Converts two's complement binary number to decimal """
     bv = bv_to_dec(raw_val)
     if bv < 0:
-        bv_val = ""
-        for c in raw_val:
-            if c == '1':
-                bv_val += '0'
-            else:
-                bv_val += '1'
-        return -1 * bv_to_dec(bv_val) - 1
+        return ones_to_dec(raw_val) - 1
     else:
         return bv
+
+
+def check_binary(raw_val: str) -> None:
+    """ Raises an ValueError if a char in the provided str is not 0 or 1 """
+    for c in raw_val:
+        if c not in ["1", "0"]:
+            raise ValueError("Please provide a binary number as str.")
+
+
+def show_all_variants(raw_val: str) -> None:
+    """ Prints out all three variants """
+    print(bv_to_dec(raw_val))
+    print(ones_to_dec(raw_val))
+    print(twos_to_dec(raw_val))
 
 
 if __name__ == "__main__":
@@ -47,6 +55,7 @@ if __name__ == "__main__":
     parser.add_argument("-n", "-number", help="The binary you want to convert", type=str)
     parser.add_argument("-k", "-kind", help="The kind of complement it is", type=str)
     args = parser.parse_args()
+    check_binary(args.n)
     match args.k:
         case "0":
             print(bv_to_dec(args.n))
@@ -54,3 +63,5 @@ if __name__ == "__main__":
             print(ones_to_dec(args.n))
         case "2":
             print(twos_to_dec(args.n))
+        case _:
+            show_all_variants(args.n)
